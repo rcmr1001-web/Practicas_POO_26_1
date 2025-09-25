68df30a8c44f44bfc1dd09980a178fb1ffe858c8
rcmr1001-web <rc.mr.1001@gmail.com>
2025-09-24 22:09:21 -0600

    Se inicia repositorio

---
+++ Assets/Assets/PRACTICA 1/CODIGOS/CONTROL BOLA.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CONTROLBOLA : MonoBehaviour
+{
+    public Rigidbody rb;  //manipulacion a lo largo del codigo el rb
+    public float fuerzaDeLanzamiento = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+             
+        }
+    }
+}
